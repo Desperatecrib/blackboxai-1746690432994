0efe3aca51338ea7536fc66734575d1d62026da7
Desperatecrib <desperatecrib@gmail.com>
2025-05-08 09:48:13 +0200

    Add backend/src/main/java/com/sacylconecta/SacylConectaApplication.java 1746690458852

---
+++ backend/src/main/java/com/sacylconecta/SacylConectaApplication.java
@@ -0,0 +1,12 @@
+package com.sacylconecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacylConectaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SacylConectaApplication.class, args);
+    }
+}
