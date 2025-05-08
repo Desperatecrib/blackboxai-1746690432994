21d88b1df15f43ce4f2ffe72bf00351551829306
Desperatecrib <desperatecrib@gmail.com>
2025-05-08 09:48:27 +0200

    Add backend/src/main/java/com/sacylconecta/repository/AppointmentRepository.java 1746690473047

---
+++ backend/src/main/java/com/sacylconecta/repository/AppointmentRepository.java
@@ -0,0 +1,10 @@
+package com.sacylconecta.repository;
+
+import com.sacylconecta.model.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByDoctorId(Long doctorId);
+    List<Appointment> findByPatientId(Long patientId);
+}
